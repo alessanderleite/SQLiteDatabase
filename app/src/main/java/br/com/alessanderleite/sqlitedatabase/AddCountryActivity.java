0ac93740a2683b422959ce698d4038355629765b
alessanderleite <alessander.logistica@gmail.com>
2019-02-06 09:10:28 -0200

    create an empty activity called AddCountryActivity

---
+++ app/src/main/java/br/com/alessanderleite/sqlitedatabase/AddCountryActivity.java
@@ -0,0 +1,12 @@
+package br.com.alessanderleite.sqlitedatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

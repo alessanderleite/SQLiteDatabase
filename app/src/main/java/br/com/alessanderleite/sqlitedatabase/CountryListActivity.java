623d75ca21916fdbf632dfa50c3c028b842e6777
alessanderleite <alessander.logistica@gmail.com>
2019-02-05 23:34:47 -0200

    code the CountryListActivity

--- app/src/main/java/br/com/alessanderleite/sqlitedatabase/CountryListActivity.java
+++ app/src/main/java/br/com/alessanderleite/sqlitedatabase/CountryListActivity.java
@@ -1,12 +1,53 @@
 package br.com.alessanderleite.sqlitedatabase;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class CountryListActivity extends AppCompatActivity {
+
+    private DBManager dbManager;
+    private ListView listView;
+    private SimpleCursorAdapter adapter;
+
+    final String[] from = new String[] {DatabaseHelper._ID, DatabaseHelper.SUBJECT, DatabaseHelper.DESC};
+    final int[] to = new int[] {R.id.id, R.id.title, R.id.desc};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_emp_list);
+
+        dbManager = new DBManager(this);
+
+        dbManager = new DBManager(this);
+        dbManager.open();
+        Cursor cursor = dbManager.fetch();
+
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setEmptyView(findViewById(R.id.empty));
+
+        adapter = new SimpleCursorAdapter(this, R.layout.activity_view_record, cursor, from, to, 0);
+        adapter.notifyDataSetChanged();
+        listView.setAdapter(adapter);
+
+        //OnClickListener For List Items
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long viewId) {
+                TextView idTextView = (TextView) view.findViewById(R.id.id);
+                TextView titleTextView = (TextView) view.findViewById(R.id.title);
+                TextView descTextView = (TextView) view.findViewById(R.id.desc);
+
+                String id = idTextView.getText().toString();
+                String title = titleTextView.getText().toString();
+                String desc = descTextView.getText().toString();
+            }
+        });
     }
 }
